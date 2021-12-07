1e7e73092306a174272c659c84ed42e8f61eb0e6
Riccardo Fonti <riccardo.fonti@studio.unibo.it>
2021-12-07 10:35:10 +0100

    add lowercase, count chars, count lines

--- src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
+++ src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
@@ -35,7 +35,10 @@ public final class LambdaFilter extends JFrame {
     private static final long serialVersionUID = 1760990730218643730L;
 
     private enum Command {
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        LOWERCASE("Lowercase", s -> s.toLowerCase()),
+        COUNT_CHARS("Count chars", s -> Long.toString(s.chars().filter(c -> c != '\n').count())),
+        COUNT_LINE("Count lines", s -> Long.toString(s.chars().filter(c -> c == '\n').count() + 1));
 
         private final String commandName;
         private final Function<String, String> fun;
